a87340ed6698be0542ec6e05aae9c906fb52e606
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-05 09:57:36 -0600

    Sección 2 - ¿Por qué TanStack Query? - Numero aleatorio criptografico atmosferico

---
+++ 01-cripto-random/src/components/shared/Loading.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@utils/cn';
+import type { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
+
+export type ILoadingProps = ComponentPropsWithoutRef<'span'> & PropsWithChildren;
+
+const Loading = ({ className }: ILoadingProps) => {
+  return <span className={cn('loading loading-spinner loading-md', className)}></span>;
+};
+
+const LoadingFullPage = () => {
+  return (
+    <div className="w-dvw h-dvh grid place-items-center">
+      <Loading />
+    </div>
+  );
+};
+
+Loading.FullPage = LoadingFullPage;
+
+export { Loading };
--- 01-cripto-random/vite.config.ts
+++ 01-cripto-random/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
 });
