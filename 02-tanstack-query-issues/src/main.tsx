d2033ee078aa258bc03aed8cbbae7c3414cae824
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-05 16:44:55 -0600

    Seccion 3 - TanStack Query - IssuesApp - Instalación - TanStack Query

--- 02-tanstack-query-issues/src/main.tsx
+++ 02-tanstack-query-issues/src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { router } from './router';
 
 import './index.css';
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+
+       <ReactQueryDevtools  />
+    </QueryClientProvider>
   </React.StrictMode>
 );
